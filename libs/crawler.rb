6089229d9a6b4ced0b1b926bd8189d1a88036042
sashimi3 <sashimisoup@gmail.com>
2019-08-26 19:50:49 +0900

    Enhance restrictions by search words(AND search)

--- libs/crawler.rb
+++ libs/crawler.rb
@@ -12,7 +12,7 @@ module EgoSearch
       Config.words.each do |word|
         @client.search(word).each do |tweet|
           emit :crawl, word, tweet
-          break if Tweet.exists? tweet.id
+          next if Tweet.exists? tweet.id
           tweet = Tweet.parse tweet
           unless @nosave
             begin
@@ -24,6 +24,10 @@ module EgoSearch
           end
           next if Config.ignore.users.find{|u| tweet.user =~ /#{u}/i }
           next if Config.ignore.words.find{|w| tweet.text =~ /#{w}/i }
+
+          # Twitterの検索APIが緩いため, ツイート本文に Config.words 中の単語をいずれも一切含まないものは弾く(wordsがスペースを含む場合はAND検索とする)
+          next if Config.words.find{|search_word| search_word.split.all?{|w| tweet.text =~ /#{w}/i } }.nil?
+
           emit :new, word, tweet
         end
       end
