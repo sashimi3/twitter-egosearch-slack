c424f978525dfbc1502213c37aa3562fcffa5f5c
sashimi3 <sashimisoup@gmail.com>
2019-08-26 19:38:12 +0900

    Customize username and icon_emoji

--- libs/sender.rb
+++ libs/sender.rb
@@ -4,6 +4,8 @@ module EgoSearch
     if Config.slack.kind_of? Hash and
         Config.slack.has_key? 'token' and
         Config.slack.has_key? 'channel'
+        Config.slack.has_key? 'username'
+        Config.slack.has_key? 'icon_emoji'
 
       Slack.configure do |config|
         config.token = Config.slack.token
@@ -14,7 +16,11 @@ module EgoSearch
     end
 
     def self.send(message)
-      @@client.chat_postMessage(channel: Config.slack.channel, text: message, as_user: true)
+      @@client.chat_postMessage(
+        channel: Config.slack.channel,
+        text: message,
+        username: Config.slack.username,
+        icon_emoji: Config.slack.icon_emoji)
     end
   end
 
